b15626d4b95358f1f745a95c55091255f50b23a4
elReclusa <howell_j@hotmail.com>
2022-10-06 14:29:59 -0500

    started pseudocode in comments in js file

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// prompts for password criteria
+
+// choose length of password 8-128 characters
+// error message if <8 or >128 length chosen 
+//"Password must contain between 8 and 128 characters." 
+
+// choose which character types to include:
+//        lowercase (abcdefghijklmnopqrstuvwxyz)
+//        uppercase (ABCDEFGHIJKLMNOPQRSTUVWXYZ)
+//        numbers (012356789)
+//        special characters (" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+
+// each prompt should be validated and at least one character type selected , 
+//"Click OK to include lowercase characters."
+//"Click OK to include UPPERCASE characters."
+//"Click OK to include numerical characters."
+//"Click OK to include special characters."
+// ie, error message if none selected "Must select at east one character type."
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,5 +31,11 @@ function writePassword() {
 
 }
 
+// password generated
+// password displayed
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
