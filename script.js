3dded9d56e7083b3729b6e7a2269bbebd8141bbb
elReclusa <howell_j@hotmail.com>
2022-10-07 17:58:42 -0500

    actual working code

--- script.js
+++ script.js
@@ -3,45 +3,59 @@ var generateBtn = document.querySelector("#generate");
 
 // prompts for password criteria
 function generatePassword() {
-  var characterLength = prompt("Enter number of characters");
-
-  console.log(characterLength);
+  var characterLength = parseInt(prompt("Enter number of characters"));
+  var password = ""
+  //console.log(characterLength);
 
   if (!characterLength || characterLength < 8 || characterLength > 128) {
     alert("Passsword must contain between 8 and 128 Characters!")
+    return 
   }
   else {
     var possibleChars = []
     var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var uppercaseLetters = lowercaseLetters.map(function (el) {
-      return el.toUpperCase();
-
-      var numbers = ["0", "1,"2","3","4","5","6","7","8","9"];
-      var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", " ^ ", "_",
+    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", ']', "^",
       ",", "`", "{", "|", "}", "~"];
 
-
+    var uppercaseLetters = lowercaseLetters.map(function (el) {
+      return el.toUpperCase();
     })
 
     var lowercaseConfirm = confirm("Would you like to use lowercase characters?");
     var uppercaseConfirm = confirm("Would you like to use uppercase letters?");
     var numbersConfirm = confirm("Would you like to use digits?");
-    var specialCharConfirm = confirm("Would you like to use special characters?");
+    var specialCharsConfirm = confirm("Would you like to use special characters?");
 
-    if (lowercaseConfirm
+    if (lowercaseConfirm) {
+      possibleChars.push(lowercaseLetters)
 
+    }
+
+    if (uppercaseConfirm) { possibleChars.push(uppercaseLetters) }
+    if (numbersConfirm) { possibleChars.push(numbers) }
+    if (specialCharsConfirm) { possibleChars.push(specialChars) }
+
+    console.log(possibleChars)
+
+    var i = 0
+    while (password.length < characterLength) {
+
+      var characterArray = possibleChars[i]
+
+      var newChar = characterArray[Math.floor(Math.random() * characterArray.length)]
+
+      password = password + newChar
+      console.log(password)
+      if (i !== possibleChars.length - 1) {
+        i++
+      }
+      else {
+        i=0
+      }
+    }
   }
-
-  console.log(characterType);
-
-  if (characterType = true) {
-    console.log()
-  } else {
-    console.log
-  }
-  for (var i = characterLength; i++) {
-    console.log()
-  }
+  return password
 }
 
 
@@ -53,7 +67,7 @@ function generatePassword() {
 
 
 // choose which character types to include:
-//        lowercase (abcdefghijklmnopqrstuvwxyz) ok or cancel 
+//        numbers (abcdefghijklmnopqrstuvwxyz) ok or cancel 
 //        uppercase (ABCDEFGHIJKLMNOPQRSTUVWXYZ) ok or cancel
 //        numbers (012356789) ok or cancel
 //        special characters (" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~") ok or cancel 
